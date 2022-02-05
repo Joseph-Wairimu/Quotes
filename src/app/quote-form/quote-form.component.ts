b7f90d812712b7e0ba8fbdaef322d7ddd5966818
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-05 12:30:04 +0300

    two way binding

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,Output,EventEmitter} from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
+  newQuote = new Quote(0,"","",new Date(),'');
+  @Output() addDiary = new EventEmitter<Quote>();
   constructor() { }
 
   ngOnInit(): void {
