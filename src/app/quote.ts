64cab2696ed277ddfdbf4a62a04b59b28227a4b4
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-05 11:28:54 +0300

    adding property to blueprinnt class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    showDescription: boolean;
+    constructor(public id: number, public author:string, public quote: string,public completeDate: Date,public name: string  ){
+      this.showDescription=false;
+    }
 }
