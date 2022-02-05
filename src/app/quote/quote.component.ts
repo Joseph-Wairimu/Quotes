cddc066b6fe5942f8b9d26a7482d592bae5b149b
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-05 11:30:26 +0300

    import blueprint class to quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote(1, 'Joseph Kamore', 'Find an online version and watch merlin find his son',new Date(2020,3,14),''),
+   
+  ];
 
   constructor() { }
 
